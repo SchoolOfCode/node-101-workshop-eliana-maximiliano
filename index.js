01c1c2e69cacb74935f7d9784624e331e78d1f4f
Eliana <elianacalote@gmail.com>
2021-12-07 10:55:01 +0000

    final commit

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
 
-  function describeItem(name, count, whatILike) {
-    console.log(`I have ${count}. ${name}. Here's what I like about them: ${whatILike}`)
-  }
+import chalk from "chalk";
+import myCollection from "./collection.js";
 
-  describeItem(myCollection[0].name, myCollection[0].count, myCollection[0].whatILike)
-
-
-  function describeCollection(name) {
-    let copyItems = []
-    name.forEach(describeItem(name)); {
-     copyItems.push(item)
-    console.log(copyItems)
+function describeItem(item) {
+  if (item.count > 1) {
+    console.log(
+      `I have ${chalk.yellow(item.count)} ${chalk.cyan(
+        item.name
+      )}s. Here's what I like about them: ${chalk.green(item.whatILike)}`
+    );
+  } else {
+    console.log(
+      `I have a ${chalk.cyan(
+        item.name
+      )}. Here's what I like about it: ${chalk.green(item.whatILike)}`
+    );
   }
 }
 
-//   const items = ['item1', 'item2', 'item3']
-// const copyItems = []
+function describeCollection(collection) {
+  collection.forEach(describeItem);
+}
 
-// // before
-// for (let i = 0; i < items.length; i++) {
-//   copyItems.push(items[i])
-// }
-
-// // after
-
-// })
-  // 2d. Now make a function called describeCollection that takes in an array as a parameter. The function should loop through the array, 
-  // and for each item, it should call the describeItem function so it displays a message according to how many you have in your collection.
-
-  // Call your function below where you've defined it, handing in the myCollection array.
-  
-  // Run the file again to see the output and check that it outputs the correct message 
-  // for each item in your collection (i.e. each object in your myCollection array).
+describeCollection(myCollection);
